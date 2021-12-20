ceda112095af81b0e64194ed2349e7aaf94e82c1
Jannis Bujak <jannis.bujak@grecon.de>
2021-12-20 18:36:15 +0100

    Finally got stuff working. Basically copied stuff from a video-tutorial: https://www.youtube.com/watch?v=tFGlLa2fwNQ
    Intervals are not right yet..

---
+++ SpraveAIAndroid/app/src/main/java/com/example/spraveaiandroid/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.spraveaiandroid;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.view.View;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void startBackgroundTask(View view) {
+        Intent intent = new Intent(this, BgTask.class);
+        intent.setAction("BGProcess");
+
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 0, intent, 0);
+
+        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, 0, 1000, pendingIntent);
+
+        finish();
+    }
+}
